4e3190f7e3d681e5b877bc63c7cad5b6b08dc5cc
Aghast365 <38799808+Aghast365@users.noreply.github.com>
2021-09-12 16:06:52 -0400

    Used a recursive function to print any number of bottles for the bottle song

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,25 @@
-function bottleSong() {
-  // Write your code here!
+function bottleSong(bottles) {
+	// Write your code here!
+	function minusOne(bottles, bottlesLeft) {
+	if (bottlesLeft < 0) {return;}
+	
+	let number = ((bottlesLeft > 0) ? bottlesLeft : ((bottlesLeft === 0) ? "no more" : bottles)).toString();
+	let plural = (bottlesLeft !== 1) ? "bottles" : "bottle";
+	
+	console.log(`${number.charAt(0).toUpperCase() + number.substring(1)} ${plural} of beer on the wall, ${number} ${plural} of beer.`);
+	
+	bottlesLeft--;
+	
+	number = (bottlesLeft > 0) ? bottlesLeft : ((bottlesLeft === 0) ? "no more" : bottles);
+	plural = (bottlesLeft !== 1) ? "bottles" : "bottle";
+	let store = (bottlesLeft >= 0) ? "Take one down and pass it around" : "Go to the store and buy some more";
+	
+	console.log(`${store}, ${number} ${plural} of beer on the wall.`);
+	
+	minusOne(bottles,bottlesLeft);
+	}
+	minusOne(bottles,bottles);
+	
 };
 
-bottleSong();
+bottleSong(99);
